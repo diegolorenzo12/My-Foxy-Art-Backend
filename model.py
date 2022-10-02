8f8be3e28617f1f9b4243b4454f0660d47ec283e
Beto-cpu <beto.munguia.lara@gmail.com>
2022-10-02 03:41:08 -0700

    initial commit

---
+++ model.py
@@ -0,0 +1,52 @@
+import random
+import os
+import tensorflow as tf
+
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+import matplotlib as mpl
+import tensorflow_hub as hub
+
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+import numpy as np
+import PIL.Image
+
+
+class Model:
+	def tensor_to_image(self, tensor):
+		tensor = tensor * 255
+		tensor = np.array(tensor, dtype=np.uint8)
+		if np.ndim(tensor) > 3:
+			assert tensor.shape[0] == 1
+			tensor = tensor[0]
+		return PIL.Image.fromarray(tensor)
+
+	def load_img(self, path_to_img):
+		max_dim = 512
+		img = tf.io.read_file(path_to_img)
+		img = tf.image.decode_image(img, channels=3)
+		img = tf.image.convert_image_dtype(img, tf.float32)
+
+		shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+		long_dim = max(shape)
+		scale = max_dim / long_dim
+
+		new_shape = tf.cast(shape * scale, tf.int32)
+
+		img = tf.image.resize(img, new_shape)
+		img = img[tf.newaxis, :]
+		return img
+
+	def generate_image(self, content_name, content_url, style_name, style_url):
+		content_path = tf.keras.utils.get_file(content_name,
+											   content_url)
+		style_path = tf.keras.utils.get_file(style_name,
+											 style_url)
+		content_image = self.load_img(content_path)
+		style_image = self.load_img(style_path)
+
+		hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+		stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
+		img = self.tensor_to_image(stylized_image)
+
+		return img
